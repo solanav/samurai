62959d71f850c0112cf43c00fe0e58c36c11389e
solanav <solanav@qq.com>
2020-01-15 23:42:42 +0100

    Added UPnP functions (fixed #2) and added port to Server.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,2 @@
-extern crate rand;
-
 pub mod types;
 pub mod network;
--- src/network/passive.rs
+++ src/network/passive.rs
@@ -1,32 +1,70 @@
 use crate::network::handler::Handler;
 use crate::network::packet::{Packet, TOTAL_SIZE};
-use std::net::UdpSocket;
+use crate::types::bucket_list::BucketList;
+use std::net::{UdpSocket, Ipv4Addr, SocketAddrV4, IpAddr};
 use std::thread;
 use std::sync::{Arc, Mutex};
 use std::collections::VecDeque;
-use crate::types::bucket_list::BucketList;
+use igd;
+use get_if_addrs;
 
 pub struct Server {
     socket: Arc<Mutex<UdpSocket>>, // Server's socket
-    num_nodes: Arc<Mutex<usize>>,  // Number of nodes we send when find_node is received
-    bucket_list: Arc<Mutex<BucketList>>, // List of peers
+    num_nodes: Arc<Mutex<usize>>, // Number of nodes we send when find_node is received
+    bucket_list: Arc<Mutex<BucketList>>, // List of buckets
     requests: Arc<Mutex<ReqList>>, // List of requests
+    port: u16, // External port
 }
 
 pub type ReqList = VecDeque<Packet>;
 
 impl Server {
     pub fn new(num_nodes: usize, requests: Arc<Mutex<ReqList>>, bucket_list: BucketList) -> Self {
+        // Look for a random local port that is not being used
         let socket: UdpSocket;
+        let internal_port: u16;
         'outer: loop {
             for p in 1024..65535 {
                 match UdpSocket::bind(format!("127.0.0.1:{}", p)) {
                     Ok(s) => {
                         socket = s;
+                        internal_port = p;
                         break 'outer;
-                    },
+                    }
                     Err(_) => {}
                 };
+            }
+        }
+
+        // Get local IP
+        let mut local_ip: Option<Ipv4Addr> = None;
+        let ip_list = get_if_addrs::get_if_addrs().unwrap();
+        for ip in ip_list.iter() {
+            match ip.ip() {
+                IpAddr::V4(ip) => {
+                    local_ip = Some(ip);
+                    break;
+                },
+                IpAddr::V6(_) => {},
+            };
+        }
+        if local_ip.is_none() {
+            panic!("Failed to get local IPv4");
+        }
+
+        // Get a random external port with UPnP
+        let external_port: u16;
+        match igd::search_gateway(Default::default()) {
+            Err(ref err) => panic!("Error: {}", err),
+            Ok(gateway) => {
+                match gateway.add_any_port(
+                    igd::PortMappingProtocol::UDP,
+                    SocketAddrV4::new(local_ip.unwrap(), internal_port),
+                    60,
+                    "Samurai") {
+                    Err(ref e) => panic!("Error getting port: {}", e),
+                    Ok(port) => external_port = port,
+                }
             }
         }
 
@@ -35,6 +73,7 @@ impl Server {
             num_nodes: Arc::new(Mutex::new(num_nodes)),
             bucket_list: Arc::new(Mutex::new(bucket_list)),
             requests,
+            port: external_port,
         }
     }
 
