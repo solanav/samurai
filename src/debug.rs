2ffeb0d73d8775581ad830b5f886c01831a671ab
solanav <solanav@qq.com>
2020-03-01 20:40:45 +0100

    Added debug_send! macro to send messages to the debug server.

--- src/debug.rs
+++ src/debug.rs
@@ -1,6 +1,14 @@
 use std::net::{IpAddr, TcpStream, SocketAddrV4, Shutdown, Ipv4Addr};
 use std::io::Write;
 
+#[macro_export]
+macro_rules! debug_send {
+    ($ip:expr, $port:expr, $msg:expr) => (
+        let ip: Ipv4Addr = std::str::FromStr::from_str($ip).unwrap();
+        samurai::debug::send_message(SocketAddrV4::new(ip, $port), $msg.to_string())
+    );
+}
+
 pub fn send_message(debug_server_addr: SocketAddrV4, msg: String) {
     let mut con = match TcpStream::connect(debug_server_addr) {
         Ok(c) => c,
